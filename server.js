b26a6a7f853a3f679e45c0362018deb814890f30
Sarat Chandra Sekhar Ryali <sarat.ryali@planonsoftware.com>
2020-06-24 23:41:28 +0530

    added support for heroku deployment

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8081;
+
 // configure our express instance with some body-parser settings
 // including handling JSON data
 app.use(bodyParser.json());
@@ -11,6 +15,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = require("./routes/routes.js")(app, fs);
 
-var server = app.listen(5000, function () {
+var server = app.listen(port, function () {
   console.log("Random Quotes listening on port %s", server.address().port);
 });
